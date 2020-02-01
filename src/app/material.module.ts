ffea78946d3dd1edad501a227d66c19235a79349
Bruno Silva_CONTR <c0603070@vale.com>
2020-02-01 01:26:47 -0200

    configuring flex layout

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule, MatIconModule } from '@angular/material';
+import {
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule
+} from '@angular/material';
 
 
 @NgModule({
-    imports: [MatIconModule, MatButtonModule],
-    exports: [MatIconModule, MatButtonModule]
+    imports: [MatIconModule, MatButtonModule, MatFormFieldModule, MatInputModule],
+    exports: [MatIconModule, MatButtonModule, MatFormFieldModule, MatInputModule]
 })
 export class MaterialModule { }
