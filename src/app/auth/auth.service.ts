649412dc0bb24e3801e6cf939b4b919b2986f4b6
Bruno Silva_CONTR <c0603070@vale.com>
2020-02-16 11:00:41 -0300

    Implementing redirect

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,18 +1,23 @@
+import { Injectable } from '@angular/core';
 import { Subject } from "rxjs/Subject";
 
 import { User } from './user.model';
 import { AuthData } from './auth-data.model';
+import { Router } from '@angular/router';
 
+@Injectable()
 export class AuthService {
     public authChange = new Subject<boolean>();
     private user: User;
+
+    constructor(private router: Router) {}
 
     registerUser(authData: AuthData) {
         this.user = {
             email: authData.email,
             userId: Math.round(Math.random() * 10000).toString()
         };
-        this.authChange.next(true);
+        this.authSuccessfully();
     }
 
     login(authData: AuthData) {
@@ -20,12 +25,14 @@ export class AuthService {
             email: authData.email,
             userId: Math.round(Math.random() * 10000).toString()
         };
-        this.authChange.next(true);
+        this.authSuccessfully();
+
     }
 
     logout() {
         this.user = null;
         this.authChange.next(false);
+        this.router.navigate(['/login']);
     }
 
     getUser() {
@@ -34,5 +41,10 @@ export class AuthService {
 
     isAuth() {
         return this.user !== null;
+    }
+
+    authSuccessfully() {
+        this.authChange.next(true);
+        this.router.navigate(['/training']);
     }
 }
